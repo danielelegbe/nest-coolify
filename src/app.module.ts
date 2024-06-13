89b1f8594725681e037d1c41ca649c635e1de1e9
Daniel Elegbe <daniel.elegbe@halfords.co.uk>
2024-06-13 05:21:16 +0100

    add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PostsModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
